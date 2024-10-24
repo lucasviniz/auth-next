ca19b33c25816ed93344ac6d050f31ab8c39ebfb
lucas vinicius <lucasivo@agespisa.com.br>
2024-10-24 10:58:29 -0300

    created login form

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -1,7 +1,9 @@
 "use client"
 
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import { Header } from "@/components/auth/header";
+import { Social } from "@/components/auth/social";
+import { BackButton } from "@/components/auth/back-button";
 
 interface CardWrapperProps {
     children: React.ReactNode;
@@ -26,6 +28,17 @@ export const CardWrapper = ({
             <CardContent>
                 {children}
             </CardContent>
+            {showSocial && (
+                <CardFooter>
+                    <Social />
+                </CardFooter>
+            )}
+            <CardFooter>
+                <BackButton 
+                    label={backButtonLabel}
+                    href={backButtonHref}
+                />
+            </CardFooter>
         </Card>
     )
 }
