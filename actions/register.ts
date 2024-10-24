8df886f9577e7e7efb926dcf7ad3be66b882b723
lucas vinicius <lucasivo@agespisa.com.br>
2024-10-24 13:26:15 -0300

    create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,10 @@
 "use server";
 
-import * as z from "zod"
-
+import bcrypt from "bcrypt";
+import * as z from "zod";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { error } from "console";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -11,5 +13,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         return { error: "Invalid fields!" };
     }
 
-    return { success: "Email sent!" };
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await db.user.findUnique({
+        where: {
+            email,
+        }
+    })
+
+    if(existingUser) {
+        return { error: "Email already in use!"}
+    }
+
+    await db.user.create({
+        data:{
+            name,
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    return { success: "User created!" };
 }
