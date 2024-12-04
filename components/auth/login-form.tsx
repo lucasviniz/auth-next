a8b41953d580c59620829ecee623dc431b07536c
lucasviniz <howaitosame@gmail.com>
2024-12-04 11:31:09 -0300

    oauth google and github

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -19,8 +19,11 @@ import { Button } from "@/components/ui/button"
 import { FormError } from "@/components/form-error"
 import { FormSuccess } from "@/components/form-success"
 import { login } from "@/actions/login"
+import { useSearchParams } from "next/navigation"
 
 export const LoginForm = () => {
+    const searchParams = useSearchParams();
+    const urlError = searchParams.get("error") === "OAuthAccountNotLinked" ? "Email already in use with different provider!" : "";
     const [isPending, startTransition] = useTransition();
     const [error, setError] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string | undefined>("");
@@ -99,7 +102,7 @@ export const LoginForm = () => {
                             )}
                         />
                     </div>
-                    <FormError message={error}/>
+                    <FormError message={error || urlError}/>
                     <FormSuccess message={success}/>
                     <Button
                         type="submit"
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -3,15 +3,24 @@
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
+
+    const onClick = (provider: "google" | "github") => {
+        signIn(provider, {
+            redirectTo: DEFAULT_LOGIN_REDIRECT,
+        })
+    }
+
     return (
         <div className="flex items-center w-full gap-x-2">
             <Button
                 size="lg"
                 className="w-full"
                 variant="outline"
-                onClick={() => {}}
+                onClick={() => onClick("google")}
             >
                 <FcGoogle />
             </Button>
@@ -20,7 +29,7 @@ export const Social = () => {
                 size="lg"
                 className="w-full"
                 variant="outline"
-                onClick={() => {}}
+                onClick={() => onClick("github")}
             >
                 <FaGithub />
             </Button>
